45615bd74502e99f0417675219e68cfab65b88a9
Haoran Qiu <haoranq4@illinois.edu>
2024-04-03 15:24:36 -0500

    added evaluation code tested with opt-350m

---
+++ scheduler/run_exp_scheduling.py
@@ -0,0 +1,69 @@
+from datasets import load_dataset
+import pandas as pd
+import math
+import numpy as np
+import time
+from vllm import LLM, SamplingParams
+
+
+model_to_serve = 'facebook/opt-350m'
+model_to_serve_short = 'opt_350m'
+dataset_path = 'sampled_prompts_responses_' + model_to_serve_short + '.csv'
+
+# load the llm query dataset from csv
+df_prompts = pd.read_csv(dataset_path)
+# print(df_prompts.head())
+print('# of total prompts:', len(df_prompts[df_prompts['model']==model_to_serve]))
+
+sampling_params = SamplingParams(temperature=0, top_p=1, max_tokens=2048)
+llm = LLM(model=model_to_serve)
+
+num_exp = 20
+num_jobs_per_exp = 50
+batch_size = 5
+use_prediction = False
+avg_jct_exp_list = []
+throughput_exp_list = []
+for exp_id in range(num_exp):
+    print('>>>> Exp #' + str(exp_id))
+    # prompts = df_prompts[df_prompts['model']==model_to_serve]['prompt'].to_list()[:num_jobs_per_exp]
+    # output_lengths = df_prompts[df_prompts['model']==model_to_serve]['response_length'].to_list()[:num_jobs_per_exp]
+    # randomly sample jobs from the dataset
+    sampled_rows = df_prompts.sample(n=num_jobs_per_exp)
+    prompts = sampled_rows['prompt'].tolist()
+    output_lengths = sampled_rows['response_length'].tolist()
+
+    if use_prediction:
+        # sort the prompts
+        dict_lst_order = {prompts[i]: output_lengths[i] for i in range(len(prompts))}
+        sorted_prompts = sorted(dict_lst_order, key=dict_lst_order.get)
+        sorted_lengths = [dict_lst_order[key] for key in sorted_prompts]
+        prompts = sorted_prompts
+        output_lengths = sorted_lengths
+
+    waiting_time = 0
+    jct_list = []
+    for i in range(math.ceil(len(prompts) / batch_size)):
+        start_time = time.perf_counter()
+        outputs = llm.generate(prompts[i * batch_size : (i+1) * batch_size], sampling_params)
+        # print('Output length (expected):', output_lengths[i * batch_size : (i+1) * batch_size])
+        # print('Output length (actual):', end=' ')
+        # for j in outputs:
+        #     print(len(j.outputs[0].token_ids), end=' ')
+        # print()
+        # generated_text = ''.join([output.outputs[i].text for i in range(len(output.outputs))])
+        end_time = time.perf_counter()
+        execution_time_ms = int((end_time - start_time) * 1000) + waiting_time
+        waiting_time = execution_time_ms
+        jct_list.append(execution_time_ms)
+    print('Avg JCT:', round(np.mean(jct_list[:-1]), 3), 'ms for executing', batch_size, 'requests')
+    # print('Individual throughput:', [batch_size * 1000 / elapsed_ms for elapsed_ms in jct_list[:-1]])
+    print('Mean throughput:', round(np.mean([batch_size * 1000 / elapsed_ms for elapsed_ms in jct_list[:-1]]), 3), 'req/s')
+    avg_jct_exp_list.append(np.mean(jct_list[:-1]))
+    throughput_exp_list.append(np.mean([batch_size * 1000 / elapsed_ms for elapsed_ms in jct_list[:-1]]))
+    print()
+
+# end of the experiments
+print('>>>> Stats over', num_exp, 'experiments:')
+print('Avg JCT:', round(np.mean(avg_jct_exp_list), 3), 'ms', '(+/-', str(round(np.std(avg_jct_exp_list), 2)) + ')')
+print('Avg throughput:', round(np.mean(throughput_exp_list), 3), 'req/s', '(+/-', str(round(np.std(throughput_exp_list), 2)) + ')')
