3e63d54572352713393b6836ce8a06d489e46f0b
Haoran Qiu <haoranq4@illinois.edu>
2024-04-03 20:23:37 -0500

    added evl results

--- scheduler/auto_eval.py
+++ scheduler/auto_eval.py
@@ -1,4 +1,3 @@
-from datasets import load_dataset
 import pandas as pd
 import math
 import numpy as np
@@ -6,20 +5,17 @@ import time
 from vllm import LLM, SamplingParams
 
 
-def eval(model_to_serve, dataset_path, max_batch_size, use_prediction=True):
-    # load the llm query dataset from csv
-    df_prompts = pd.read_csv(dataset_path)
-    # print(df_prompts.head())
-    print('# of total prompts:', len(df_prompts[df_prompts['model']==model_to_serve]))
-
-    sampling_params = SamplingParams(temperature=0, top_p=1, max_tokens=2048)
-    llm = LLM(model=model_to_serve)
-
+def eval(df_prompts, llm, max_batch_size, methods=['fcfs', 'sjf']):
     num_exp = 20
     num_jobs_per_exp = 50
     use_prediction = False
-    avg_jct_exp_list = []
-    throughput_exp_list = []
+    avg_jct_exp_list = {}
+    throughput_exp_list = {}
+
+    for method in methods:
+        avg_jct_exp_list[method] = []
+        throughput_exp_list[method] = []
+
     for exp_id in range(num_exp):
         print('>>>> Exp #' + str(exp_id))
         # randomly sample jobs from the dataset
@@ -27,61 +23,92 @@ def eval(model_to_serve, dataset_path, max_batch_size, use_prediction=True):
         prompts = sampled_rows['prompt'].tolist()
         output_lengths = sampled_rows['response_length'].tolist()
 
-        if use_prediction:
-            # sort the prompts
-            dict_lst_order = {prompts[i]: output_lengths[i] for i in range(len(prompts))}
-            sorted_prompts = sorted(dict_lst_order, key=dict_lst_order.get)
-            sorted_lengths = [dict_lst_order[key] for key in sorted_prompts]
-            prompts = sorted_prompts
-            output_lengths = sorted_lengths
+        for method in methods:
+            print('Starting experiments for', method)
+            if method == 'sjf':
+                # sort the prompts
+                dict_lst_order = {prompts[i]: output_lengths[i] for i in range(len(prompts))}
+                sorted_prompts = sorted(dict_lst_order, key=dict_lst_order.get)
+                sorted_lengths = [dict_lst_order[key] for key in sorted_prompts]
+                prompts = sorted_prompts
+                output_lengths = sorted_lengths
 
-        waiting_time = 0
-        jct_list = []
-        num_completed_jobs = []
-        for i in range(math.ceil(len(prompts) / max_batch_size)):
-            start_time = time.perf_counter()
-            outputs = llm.generate(prompts[i * max_batch_size : (i+1) * max_batch_size], sampling_params)
-            num_completed_jobs.append(len(outputs))
-            end_time = time.perf_counter()
-            execution_time_ms = int((end_time - start_time) * 1000) + waiting_time
-            waiting_time = execution_time_ms
-            jct_list.append(execution_time_ms)
-        avg_jct = round(np.average(jct_list, weights=num_completed_jobs), 3)
-        print('Avg JCT:', avg_jct, 'ms for executing', round(np.mean(num_completed_jobs), 2), 'requests')
-        avg_throughput = round(np.mean([num_jobs * 1000 / elapsed_ms for elapsed_ms, num_jobs in zip(jct_list, num_completed_jobs)]), 3)
-        print('Mean throughput:', avg_throughput, 'req/s')
-        avg_jct_exp_list.append(avg_jct)
-        throughput_exp_list.append(avg_throughput)
-        print()
+            waiting_time = 0
+            jct_list = []
+            num_completed_jobs = []
+            for i in range(math.ceil(len(prompts) / max_batch_size)):
+                start_time = time.perf_counter()
+                outputs = llm.generate(prompts[i * max_batch_size : (i+1) * max_batch_size], sampling_params)
+                num_completed_jobs.append(len(outputs))
+                end_time = time.perf_counter()
+                execution_time_ms = int((end_time - start_time) * 1000) + waiting_time
+                waiting_time = execution_time_ms
+                jct_list.append(execution_time_ms)
+            avg_jct = round(np.average(jct_list, weights=num_completed_jobs), 3)
+            print('Avg JCT:', avg_jct, 'ms for executing', round(np.mean(num_completed_jobs), 2), 'requests')
+            avg_throughput = round(np.mean([num_jobs * 1000 / elapsed_ms for elapsed_ms, num_jobs in zip(jct_list, num_completed_jobs)]), 3)
+            print('Mean throughput:', avg_throughput, 'req/s')
+            avg_jct_exp_list[method].append(avg_jct)
+            throughput_exp_list[method].append(avg_throughput)
+            print()
 
     # end of the experiments
-    print('>>>> Stats over', num_exp, 'experiments:')
-    print('Avg JCT:', round(np.mean(avg_jct_exp_list), 3), 'ms', '(+/-', str(round(np.std(avg_jct_exp_list), 2)) + ')')
-    print('Avg throughput:', round(np.mean(throughput_exp_list), 3), 'req/s', '(+/-', str(round(np.std(throughput_exp_list), 2)) + ')')
-    return round(np.mean(avg_jct_exp_list), 3), round(np.mean(throughput_exp_list), 3)
+    print('>>>> Statistics over', num_exp, 'experiments:')
+    ret_avg_jct = {}
+    ret_avg_throughput = {}
+    for method in methods:
+        print(method)
+        print('Avg JCT:', round(np.mean(avg_jct_exp_list[method]), 3), 'ms', '(+/-', str(round(np.std(avg_jct_exp_list[method]), 2)) + ')')
+        print('Avg throughput:', round(np.mean(throughput_exp_list[method]), 3), 'req/s', '(+/-', str(round(np.std(throughput_exp_list[method]), 2)) + ')')
+        ret_avg_jct[method] = round(np.mean(avg_jct_exp_list[method]), 3)
+        ret_avg_throughput[method] = round(np.mean(throughput_exp_list[method]), 3)
+    print()
+    return ret_avg_jct, ret_avg_throughput
 
 
 if __name__ == '__main__':
     df = pd.DataFrame(columns=['model', 'max_batch_size', 'method', 'jct', 'throughput'])
     data = []
+    methods = ['fcfs', 'sjf']
 
     model_to_serve = 'facebook/opt-350m'
     model_to_serve_short = 'opt_350m'
     dataset_path = 'sampled_prompts_responses_' + model_to_serve_short + '.csv'
+
+    # load the llm query dataset from csv
+    df_prompts = pd.read_csv(dataset_path)
+    # print(df_prompts.head())
+    print('# of total prompts:', len(df_prompts[df_prompts['model']==model_to_serve]))
+
+    sampling_params = SamplingParams(temperature=0, top_p=1, max_tokens=2048)
+    llm = LLM(model=model_to_serve, gpu_memory_utilization=0.9)
+
     for max_batch_size in [5, 10, 15, 20, 25]:
-        jct, throughput = eval(model_to_serve, dataset_path, max_batch_size, use_prediction=False)
-        data.append({'model': 'opt-350m', 'max_batch_size': max_batch_size, 'method': 'fcfs', 'jct': jct, 'throughput': throughput})
-        jct, throughput = eval(model_to_serve, dataset_path, max_batch_size, use_prediction=True)
-        data.append({'model': 'opt-350m', 'max_batch_size': max_batch_size, 'method': 'sjf', 'jct': jct, 'throughput': throughput})
+        print('Max batch size:', max_batch_size)
+        jct, throughput = eval(df_prompts, llm, max_batch_size, methods=methods)
+        for method in methods:
+            data.append({'model': 'opt-350m', 'max_batch_size': max_batch_size, 'method': method, 'jct': jct[method], 'throughput': throughput[method]})
+
+    # clear the GPU memory used by the model
+    del llm.llm_engine.driver_worker
 
     model_to_serve = 'openai-community/gpt2-medium'
     model_to_serve_short = 'gpt2_medium'
     dataset_path = 'sampled_prompts_responses_' + model_to_serve_short + '.csv'
+
+    # load the llm query dataset from csv
+    df_prompts = pd.read_csv(dataset_path)
+    # print(df_prompts.head())
+    print('# of total prompts:', len(df_prompts[df_prompts['model']==model_to_serve]))
+
+    sampling_params = SamplingParams(temperature=0, top_p=1, max_tokens=2048)
+    llm = LLM(model=model_to_serve, gpu_memory_utilization=0.9)
+
     for max_batch_size in [5, 10, 15, 20, 25]:
-        jct, throughput = eval(model_to_serve, dataset_path, max_batch_size, use_prediction=False)
-        data.append({'model': 'opt-350m', 'max_batch_size': max_batch_size, 'method': 'fcfs', 'jct': jct, 'throughput': throughput})
-        jct, throughput = eval(model_to_serve, dataset_path, max_batch_size, use_prediction=True)
-        data.append({'model': 'opt-350m', 'max_batch_size': max_batch_size, 'method': 'sjf', 'jct': jct, 'throughput': throughput})
+        print('Max batch size:', max_batch_size)
+        jct, throughput = eval(df_prompts, llm, max_batch_size, methods=methods)
+        for method in methods:
+            data.append({'model': 'gpt2-medium', 'max_batch_size': max_batch_size, 'method': method, 'jct': jct[method], 'throughput': throughput[method]})
 
     df = pd.concat([df, pd.DataFrame(data)], ignore_index=True)
     print(df.head())
